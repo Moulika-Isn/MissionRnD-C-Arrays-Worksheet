be3ea692a4fb9e4c07428f43027eb87fbe276db2
Moulika-Isn <Moulika-Isn@missionrnd.com>
2016-03-06 11:59:53 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -14,7 +14,60 @@ NOTES:
 
 #include <stdio.h>
 
+void swap(int *p, int *q)
+{
+	int temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
+void find(int s[], int n, int *x, int *y)
+{
+	int i, z = 1, j;
+	for (i = n-1; i > 0; i--)
+	{
+		if (s[i] < s[i - 1])
+		{
+			j = i - 1;
+			while (j >= 0 && s[i] < s[j])
+				j--;
+			*x = i;
+			*y = j + 1;
+			break;
+		}
+	}
+
+}
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
-	return NULL;
+	int i, x, y, c = 0;
+	if ((Arr == NULL) || (len < 1))
+		return NULL;
+	else
+	{
+		for (i = 0; i < len - 1; i++)
+		{
+			if (*(Arr + i) > *(Arr + i + 1))
+			{
+				c = 1;
+				break;
+			}
+		}
+		if (c == 0)
+			return Arr;
+		else
+		{
+			if ((*Arr > *(Arr + 1)) && (*(Arr + len - 2) > (*(Arr + len - 1))))
+			{
+				x = 0;
+				y = len - 1;
+			}
+			else
+			{
+				find(Arr, len, &x, &y);
+			}
+			swap((Arr + x), (Arr + y));
+			return Arr;
+		}
+	}
 }
